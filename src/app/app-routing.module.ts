d1082556e7dfb72df1e6587b73441e034f6204f7
Piotr Putiacycki <jedi@piotrs-macbook-air.home>
2021-04-27 17:54:21 +0200

    contact first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewsComponent } from './components/news/news.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'test', component: TestComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'news', component: NewsComponent}
+  {path: 'news', component: NewsComponent},
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
