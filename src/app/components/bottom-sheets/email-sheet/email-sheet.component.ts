2fb0d8608c7492cf669cc176a22f9e691c883458
Piotr Putiacycki <jedi@piotrs-macbook-air.home>
2021-04-28 12:58:15 +0200

    first formcontroll

--- src/app/components/bottom-sheets/email-sheet/email-sheet.component.ts
+++ src/app/components/bottom-sheets/email-sheet/email-sheet.component.ts
@@ -3,6 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-email-sheet',
@@ -12,6 +13,22 @@ import { map, shareReplay } from 'rxjs/operators';
 export class EmailSheetComponent implements OnInit {
   
   email = new FormControl('', [Validators.required, Validators.email]);
+  message = new FormControl('', [Validators.maxLength(1000)])
+
+  
+  submit(): void {
+    console.log("message validation: " + this.message.valid)
+    console.log("submit done")
+    console.log(this.email.valid);
+    this._bottomSheetRef.dismiss();
+  }
+  
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
   
   isHandset$: Observable<boolean> = this.breakpointObserver
   .observe(Breakpoints.Handset)
@@ -20,18 +37,8 @@ export class EmailSheetComponent implements OnInit {
     shareReplay()
   );
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-
-
-
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver,
+    private _bottomSheetRef: MatBottomSheetRef<EmailSheetComponent>) { }
 
   ngOnInit(): void {
   }
